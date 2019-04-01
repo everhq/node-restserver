387c1ff53e57e9aa4f1a594e4cef5afdea41754c
Ever <everhq1214@gmail.com>
2019-04-01 00:09:43 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,23 @@
 //=========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//=========================
+//Entorno
+//=========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=========================
+//Base de datos
+//=========================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cafe-user:cafe-user@cluster0-pu0wy.mongodb.net/cafe?retryWrites=true';
+}
+
+process.env.URLDB = urlDB;
