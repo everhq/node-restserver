662edbc7536afd4d9a53668fbdd64484ba7f0850
Ever <everhq1214@gmail.com>
2019-04-01 16:36:16 -0500

    paquetes acualizados

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 app.use(require('./routes/usuario'));
 
 
-mongoose.connect(process.env.URLDB, (err, res) => {
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
     if (err) throw err;
 
     console.log('base de datos online');
