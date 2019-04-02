56952f0f976bfe43680156e777652b8429c8efd7
Ever <everhq1214@gmail.com>
2019-04-01 19:41:43 -0500

    Vaiable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:cafe-user@cluster0-pu0wy.mongodb.net/cafe?retryWrites=true';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
