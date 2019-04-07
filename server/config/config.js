4f0651be4d74518904136d6fac43483e4f9ffb70
Ever <everhq1214@gmail.com>
2019-04-06 20:13:11 -0500

    Login normal

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,22 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //=========================
+//Vencimiento del token
+//=========================
+//60 segundos
+//60 min
+//24 hr
+//30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+//=========================
+//SEED de autenticación
+//=========================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+
+//=========================
 //Base de datos
 //=========================
 
